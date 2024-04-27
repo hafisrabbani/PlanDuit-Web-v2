e737f67b871e6c3ff75d68832a61120178f27e9f
hafisrabbani <hafis.gov@gmail.com>
2024-04-27 21:07:19 +0700

    adding landing page

--- components/organisms/HeroSection.vue
+++ components/organisms/HeroSection.vue
@@ -1,14 +1,16 @@
 <template>
   <ContainerAtom container-class="wow fadeInUp">
     <div class="hiro-header-carousel carousel slide" data-ride="carousel" data-interval="3500">
-      <div class="row">
+      <RowContentCenter>
         <div class="col-md-6 order-md-2 text-center mb-5">
           <ImageAtom alt="hero-image" src="/images/logo-planduit.jpg" image-class="img-fluid" />
         </div>
         <div class="col-md-6 order-md-1">
           <HeroContent />
         </div>
-      </div>
+      </RowContentCenter>
     </div>
   </ContainerAtom>
 </template>
+<script setup lang="ts">
+</script>
