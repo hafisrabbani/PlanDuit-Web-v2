8f376b90cfae5dd572899cfb375f93b3ae4e8970
hafisrabbani <hafis.gov@gmail.com>
2024-05-14 14:55:04 +0700

    fix

--- constant/feature.constant.ts
+++ constant/feature.constant.ts
@@ -9,8 +9,8 @@ KALKULATOR:[
         list: ["Zakat Perdagangan","Zakat Penghasilan","Zakat Emas","Zakat Pertanian","Zakat Tabungan"]
     },
     {
-        title_kalkulator: "Tips & Trik",
-        list: ["Jelajahi fitur penuh tips dan trik kami yang menjadi sumber informasi andalan. Dapatkan panduan praktis dan strategis yang akan membantu Anda mengelola investasi dengan lebih efektif, membimbing langkah-langkah Anda menuju kesuksesan finansial."]
+        title_kalkulator: "PlanDuit Academy",
+        list: ["Planduit Learning Center","Planduit Dictionary"]
     },
 ],
 }
